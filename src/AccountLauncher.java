7730899d203596f9b3ba2335755b7cf327d39765
WilliamAvHolmberg <30773487+WilliamAvHolmberg@users.noreply.github.com>
2018-10-10 16:13:45 +0200

    Initial commit

---
+++ src/AccountLauncher.java
@@ -0,0 +1,90 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.lang.ProcessBuilder.Redirect;
+import java.util.Locale;
+
+public final class AccountLauncher {
+	
+	public static enum OSType {
+		Windows, MacOS, Linux;
+	};
+
+	protected static OSType detectedOS;
+
+	public static OSType getOperatingSystemType() {
+		if (detectedOS == null) {
+			String OS = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
+			if ((OS.indexOf("mac") >= 0) || (OS.indexOf("darwin") >= 0)) {
+				detectedOS = OSType.MacOS;
+			} else if (OS.indexOf("win") >= 0) {
+				detectedOS = OSType.Windows;
+			} else
+				detectedOS = OSType.Linux;
+		}
+		return detectedOS;
+	}
+
+public static void launchClient(String path, String script, String clientUser, String clientPass, String botUser, String botPass, String world, String proxy, String params) {
+		System.out.println("Starting");
+		ProcessBuilder linuxBuilder = new ProcessBuilder("/bin/bash", "-c",
+	            "java -jar " + path + " -Xmx750m -allow norandoms" + proxy + " -login " + clientUser + ":" + clientPass + " -bot " + botUser + ":" + botPass + ":1234" + " -world " + world + " -script " + script + ":" + params);
+		
+		ProcessBuilder windowsBuilder = new ProcessBuilder("cmd.exe", "/c",
+				 "java -jar " + path + " -Xmx750m -allow norandoms" + proxy + " -login " + clientUser + ":" + clientPass + " -bot " + botUser + ":" + botPass + ":1234" + " -world " + world + " -script " + script + ":" + params);
+		windowsBuilder.inheritIO();
+		ProcessBuilder macBuilder = new ProcessBuilder("osascript", "-e",
+	            "tell application \"Terminal\" to do script \"java -jar " + path + " -Xmx750m -allow norandoms" + proxy + " -login " + clientUser + ":" + clientPass + " -bot " + botUser + ":" + botPass + ":1234" + " -world " + world + " -script " + script + ":" + params + "\"");
+
+		try {
+			switch(AccountLauncher.getOperatingSystemType()){
+				case Windows:
+					int i = 1;
+					System.out.println("Start windows");
+					Process p1 = windowsBuilder.start();
+					setOutputStream(p1);
+					
+				
+					
+				
+					break;
+				case MacOS:
+					Process p2 = macBuilder.start();
+					
+					break;
+				case Linux:
+					Process p3 = linuxBuilder.start();
+					break;
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			System.out.println(e1.getMessage());
+		}
+		
+
+	}
+
+public static void setOutputStream(Process process) {
+	
+    new Thread(new Runnable() {
+        @Override public void run() {
+            InputStream is = process.getInputStream();
+            int c ;
+            try {
+                while((c = is.read()) >= 0) {
+                    System.out.println(c);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+          
+        }
+    }).start();
+
+}
+}
+
+//Your proxy string will have to include " -proxy ip:port", I did it like that so it can be optional.
---
+++ src/NexHelper.java
@@ -0,0 +1,75 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class NexHelper {
+	public static void main(String[] args) {
+		new NexHelper();
+	}
+	
+	public NexHelper() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter IP");
+		String ip = sc.nextLine();
+		System.out.println("Enter Port");
+		int port = sc.nextInt();
+		
+		try {
+			Socket socket = new Socket(ip, port);
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+			out.println("computer:0");
+			if(in.readLine().equals("connected:1")) {
+				System.out.println("NexHelper has been initialized towards Nexus");
+			}else {
+				System.out.println("Connection Towards Nexus failed");
+			}
+
+			String nextRequest;
+			
+			while (true) {				
+				System.out.println("Ready to take new instructions...(account_request to start new client)");
+				nextRequest = sc.nextLine();
+				switch(nextRequest) {
+				case "account_request":
+					newAccountRequest(out, in);
+					break;
+				default:
+					out.println(nextRequest + ":0");
+					System.out.println(in.readLine());
+					break;
+				}
+			}
+		}catch (Exception e) {
+			
+		}
+	}
+
+	/*
+	 * respond index
+	 * 0 - Type of respond
+	 * 1 - Status (0 == failed, 1 == success)
+	 * 2 - login
+	 * 3 - password
+	 * 4 - proxy
+	 */
+	private void newAccountRequest(PrintWriter out, BufferedReader in) throws IOException {
+		out.println("account_request:0");
+
+		String[] respond = in.readLine().split(":");
+		if(respond[0].equals("account_request") && respond[1].equals("1")) {
+			String login = respond[2];
+			String password = respond[3];
+			String proxy = respond[4];
+			System.out.println(respond);
+			AccountLauncher.launchClient("./osbot.jar", "BDK", "wavh", "Lifeosbotbook123", login, password, "305", proxy, null);
+		}else {
+			System.out.println("No Account available atm. Try again in 5 minutes");
+		}
+	}
+}
