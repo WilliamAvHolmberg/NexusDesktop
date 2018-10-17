74d2304c737c7961018ecb7bab765ef8d3e37e0a
William Holmberg <william.av.holmberg@gmail.com>
2018-10-17 15:25:56 +0200

    added some functionality

--- src/AccountLauncher.java
+++ src/AccountLauncher.java
@@ -8,7 +8,7 @@ import java.lang.ProcessBuilder.Redirect;
 import java.util.Locale;
 
 public final class AccountLauncher {
-	
+
 	public static enum OSType {
 		Windows, MacOS, Linux;
 	};
@@ -28,63 +28,68 @@ public final class AccountLauncher {
 		return detectedOS;
 	}
 
-public static void launchClient(String path, String script, String clientUser, String clientPass, String botUser, String botPass, String world, String proxy, String params) {
+	public static void launchClient(String path, String script, String clientUser, String clientPass, String botUser,
+			String botPass, String world, String proxy, String params) {
 		System.out.println("Starting");
 		ProcessBuilder linuxBuilder = new ProcessBuilder("/bin/bash", "-c",
-	            "java -jar " + path + " -Xmx750m -allow norandoms" + proxy + " -login " + clientUser + ":" + clientPass + " -bot " + botUser + ":" + botPass + ":1234" + " -world " + world + " -script " + script + ":" + params);
-		
+				"java -jar " + path + " -Xmx750m -allow norandoms" + proxy + " -login " + clientUser + ":" + clientPass
+						+ " -bot " + botUser + ":" + botPass + ":1234" + " -world " + world + " -script " + script + ":"
+						+ params);
+
 		ProcessBuilder windowsBuilder = new ProcessBuilder("cmd.exe", "/c",
-				 "java -jar " + path + " -Xmx750m -allow norandoms" + proxy + " -login " + clientUser + ":" + clientPass + " -bot " + botUser + ":" + botPass + ":1234" + " -world " + world + " -script " + script + ":" + params);
+				"java -jar " + path + " -Xmx750m -allow norandoms" + proxy + " -login " + clientUser + ":" + clientPass
+						+ " -bot " + botUser + ":" + botPass + ":1234" + " -world " + world + " -script " + script + ":"
+						+ params);
 		windowsBuilder.inheritIO();
 		ProcessBuilder macBuilder = new ProcessBuilder("osascript", "-e",
-	            "tell application \"Terminal\" to do script \"java -jar " + path + " -Xmx750m -allow norandoms" + proxy + " -login " + clientUser + ":" + clientPass + " -bot " + botUser + ":" + botPass + ":1234" + " -world " + world + " -script " + script + ":" + params + "\"");
+				"tell application \"Terminal\" to do script \"java -jar " + path + " -Xmx750m -allow norandoms" + proxy + " -login " + clientUser + ":" + clientPass
+				+ " -bot " + botUser + ":" + botPass + ":1234" + " -world " + world + " -script " + script + ":"
+				+ params +  "\"");
 
 		try {
-			switch(AccountLauncher.getOperatingSystemType()){
-				case Windows:
-					int i = 1;
-					System.out.println("Start windows");
-					Process p1 = windowsBuilder.start();
-					setOutputStream(p1);
-					
-				
-					
-				
-					break;
-				case MacOS:
-					Process p2 = macBuilder.start();
-					
-					break;
-				case Linux:
-					Process p3 = linuxBuilder.start();
-					break;
+			switch (AccountLauncher.getOperatingSystemType()) {
+			case Windows:
+				int i = 1;
+				System.out.println("Start windows");
+				Process p1 = windowsBuilder.start();
+				setOutputStream(p1);
+
+				break;
+			case MacOS:
+				Process p2 = macBuilder.start();
+
+				break;
+			case Linux:
+				Process p3 = linuxBuilder.start();
+				break;
 			}
 		} catch (IOException e1) {
 			e1.printStackTrace();
 			System.out.println(e1.getMessage());
 		}
-		
 
 	}
 
-public static void setOutputStream(Process process) {
-	
-    new Thread(new Runnable() {
-        @Override public void run() {
-            InputStream is = process.getInputStream();
-            int c ;
-            try {
-                while((c = is.read()) >= 0) {
-                    System.out.println(c);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-          
-        }
-    }).start();
+	public static void setOutputStream(Process process) {
 
-}
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				InputStream is = process.getInputStream();
+				int c;
+				try {
+					while ((c = is.read()) >= 0) {
+						System.out.println(c);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+		}).start();
+
+	}
 }
 
-//Your proxy string will have to include " -proxy ip:port", I did it like that so it can be optional.
+// Your proxy string will have to include " -proxy ip:port", I did it like that
+// so it can be optional.
--- src/NexHelper.java
+++ src/NexHelper.java
@@ -13,20 +13,21 @@ public class NexHelper {
 	Stack<String> messageQueue;
 	long lastLog = 0;
 	private String respond = "none";
+
 	public static void main(String[] args) {
 		new NexHelper();
 	}
-	
+
 	public NexHelper() {
 		messageQueue = new Stack<String>();
-		//messageQueue.push("account_request");
-		
+		// messageQueue.push("account_request");
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter IP");
 		String ip = sc.nextLine();
 		System.out.println("Enter Port");
 		int port = sc.nextInt();
-		
+
 		try {
 			Socket socket = new Socket(ip, port);
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
@@ -35,53 +36,64 @@ public class NexHelper {
 			initializeContactToSocket(out, in);
 
 			String nextRequest;
-			
-			while (true) {				
+
+			while (true) {
 				System.out.println("Ready to take new instructions...last respond:" + respond);
-				if(!messageQueue.isEmpty()) {
+				if (!messageQueue.isEmpty()) {
 					nextRequest = messageQueue.pop();
-					switch(nextRequest) {
+					String[] parsed = nextRequest.split(":");
+					switch (parsed[0]) {
 					case "account_request":
-						newAccountRequest(out, in);
+						/*
+						 * Argument 0 == respond
+						 * Argument 1 == 0 equals that we shall ask database for a new account
+						 * Argument 1 == 1 equals that we shall use provided details to start a new client
+						 */
+						if (parsed[1].equals("0")) {
+							newAccountRequest(out, in);
+						} else if (parsed[1].equals("1")) {
+							startAccount(parsed);
+						}
 						break;
 					default:
-						log(out,in);
+						log(out, in);
 						break;
 					}
-				}else {
-					log(out,in);
+				} else {
+					log(out, in);
 					Thread.sleep(1000);
 				}
 			}
-		}catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
 
 	private void log(PrintWriter out, BufferedReader in) throws InterruptedException, IOException {
-		if(System.currentTimeMillis() - lastLog > 5000) { //only log every 5 sec
-		out.println("log:0");
-		respond = in.readLine();
-		//standard message is 'logged:fine'
-		//if respond is anything else than logged:fine we can assume it is a new instruction
-		if(!respond.equals("logged:fine")) {
-			System.out.println("we got a new instructionToQueue:" + respond);
-			messageQueue.push(respond);
+		if (System.currentTimeMillis() - lastLog > 5000) { // only log every 5 sec
+			out.println("log:0");
+			respond = in.readLine();
+			// standard message is 'logged:fine'
+			// if respond is anything else than logged:fine we can assume it is a new
+			// instruction
+			if (!respond.equals("logged:fine")) {
+				System.out.println("we got a new instructionToQueue:" + respond);
+				messageQueue.push(respond);
+			}
+			lastLog = System.currentTimeMillis();
 		}
-		lastLog = System.currentTimeMillis();
-		}
-		
+
 	}
 
 	private void initializeContactToSocket(PrintWriter out, BufferedReader in) throws IOException {
-		out.println("computer:1:" + getIP() + ":william_home_computer_test_1");
-		if(in.readLine().equals("connected:1")) {
+		out.println("computer:1:" + getIP() + ":adam_computer");
+		if (in.readLine().equals("connected:1")) {
 			System.out.println("NexHelper has been initialized towards Nexus");
-		}else {
+		} else {
 			System.out.println("Connection Towards Nexus failed");
 		}
 	}
-	
+
 	public String getIP() {
 		URL url;
 		try {
@@ -97,27 +109,28 @@ public class NexHelper {
 	}
 
 	/*
-	 * respond index
-	 * 0 - Type of respond
-	 * 1 - Status (0 == failed, 1 == success)
-	 * 2 - login
-	 * 3 - password
-	 * 4 - proxy
+	 * respond index 0 - Type of respond 1 - Status (0 == failed, 1 == success) 2 -
+	 * login 3 - password 4 - proxy
 	 */
 	private void newAccountRequest(PrintWriter out, BufferedReader in) throws IOException {
 		out.println("account_request:0");
 
 		String[] respond = in.readLine().split(":");
-		if(respond[0].equals("account_request") && respond[1].equals("1")) {
-			String login = respond[2];
-			String password = respond[3];
-			String proxy = respond[4];
-			String world = respond[5];
-			String script = respond[6];
-			System.out.println(respond);
-			AccountLauncher.launchClient("./osbot.jar", script, "wavh", "Lifeosbotbook123", login, password, world, proxy, null);
-		}else {
+		if (respond[0].equals("account_request") && respond[1].equals("1")) {
+			startAccount(respond);
+		} else {
 			System.out.println("No Account available atm. Try again in 5 minutes");
 		}
+	}
+
+	private void startAccount(String[] respond) {
+		String login = respond[2];
+		String password = respond[3];
+		String proxy = respond[4];
+		String world = respond[5];
+		String script = respond[6];
+		System.out.println(respond);
+		AccountLauncher.launchClient("./osbot.jar", script, "wavh", "Lifeosbotbook123", login, password, world, proxy,
+				null);
 	}
 }
